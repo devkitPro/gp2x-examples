640e629d954190b2052b45517269095d230e7d77
Adan Scotney <adan.scotney@gmail.com>
2020-06-12 17:04:57 +0100

    Examples tidy up

--- interpreter/source/main.c
+++ interpreter/source/main.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]) {
   gp2xInit();
 
   for(int i = 0 ; i < argc ; i++) {
-    uart_printf("argv[%d]: %s", i, argv[i]);
+    uartPrintf("argv[%d]: %s", i, argv[i]);
   }
   
   while(1);
