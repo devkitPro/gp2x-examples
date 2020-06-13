000a4b4fccdf211c747f097267bf359a3b19a4ed
Adan Scotney <adan.scotney@gmail.com>
2020-06-13 13:16:08 +0100

    Beefier 940 example - able to control FB based on button pressed, from each core independently

--- secondcpu/arm920/source/main.c
+++ secondcpu/arm920/source/main.c
@@ -9,17 +9,34 @@ int main(int argc, char* argv[]) {
 
   uartPrintf("Init ARM940\n");
   arm940Init(ARM940_MEMORY_BANK);
+  arm920Data[2] = 1;
 
   uartPrintf("Is ARM940 running? %s\n", arm940Running() ? "Yes" : "No");
 
-  //  arm940Halt();
-  //  usleep(1000000);
   arm940Run();
 
   uartPrintf("Is ARM940 running? %s\n", arm940Running() ? "Yes" : "No");
 
-  while(1) {
-    uartPrintf("Still running 0x%x\n", *((uint32_t*)0x3000000));
-    usleep(1000000);
+  uartPrintf("Waiting for ARM940 to allocate framebuffer\n");
+  while(arm920Data[2]); // wait for sync flag from 940
+  uint16_t* fb = arm9x0ImportPointer(arm920Data[0] << 16 | arm920Data[1]);
+  uartPrintf("ARM940 has allocated framebuffer at 0x%x\n", fb);
+
+  rgbSetPixelFormat(RGB565);
+  rgbRegionNoBlend(REGION1);
+  rgbSetRegionPosition(REGION1, 0, 0, 320, 240);
+  for(int i = 320*240 ; i-- ; ) {
+    *(fb+i) = 0x7861;
   }
+  rgbSetFbAddress((void*)fb);
+  rgbToggleRegion(REGION1, true);
+
+  while(1) {
+    if(btnState()&A) {
+      uartPrintf("pressed a, 920 is handling this\n");
+      for(int i = 320*240 ; i-- ; ) {
+	*(fb+i) = 0x7861;
+      }
+  }
+}
 }
--- secondcpu/arm940/source/main.c
+++ secondcpu/arm940/source/main.c
@@ -1,13 +1,26 @@
 // this doesn't do much, it simply proves the 940 is running
 #include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <orcus.h>
 
-volatile uint32_t* dest = (uint32_t*) 0x0;
-
+extern volatile void* __is_arm940;
 int main() {
-    *dest = 0x87654321;
+  void* fb = (void*)0x10000;// malloc(320*240*2); // TODO - investigate why malloc sometimes doesn't work
+  
+  arm920Data[0] = (((uint32_t)fb) >> 16);
+  arm920Data[1] = (((uint32_t)fb)&0xFFFF);
+  usleep(1000000);
+  arm920Data[2] = 0; // clear flag to indicate we've done our work
+
+  uint32_t btn = btnState();
   while(1) {
-    *dest = *dest - 1;
-    uartPrintf("hello from 940\n");
+    btn = btnState();
+    if(btn & B) {
+      uartPrintf("pressed b, 940 is handling this\n");
+      for(int i = 320*240 ; i-- ; ) {
+	*(((uint16_t*)fb)+i) = 0xD6A4;
+      }
+    }
   }
 }
