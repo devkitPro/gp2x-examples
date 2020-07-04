3d9a4afdde2f02aef5a06cd090a1c7ab1763da34
Adan Scotney <adan.scotney@gmail.com>
2020-07-04 15:53:44 +0100

    Add to audio example with switching between speakers and headphones on F100

--- audio/source/main.c
+++ audio/source/main.c
@@ -15,15 +15,32 @@ void fillBuffer(int16_t* buffer, int period) {
   }
 }
 
+void handleHeadphones() {
+  static bool wereConnected = false;
+  bool areConnected = audioHeadphonesConnected();
+  if(areConnected && !wereConnected) {
+    audioMuteSpeakers();
+    audioUnmuteHeadphones();
+    wereConnected = !wereConnected;
+  } else if(!areConnected && wereConnected) {
+    audioMuteHeadphones();
+    audioUnmuteSpeakers();
+    wereConnected = !wereConnected;
+  }
+}
+
 int main(int argc, char* argv[]) {
   gp2xInit();
   uartPrintf("Audio example\n");
   
   int16_t* buffer = (int16_t*) malloc(0xFFFF);
   audioInit(0);
-  
-  audioUnmuteHeadphones();
-  audioMuteSpeakers();
+  audioUnmuteSpeakers();
+  // In a real example you may wish to do this check in your main loop to capture
+  // headphones being unplugged/plugged back in. It will always return false on an F200 because
+  // the audio signals are physically switched. You can't run it while a sample is playing
+  // though as the AC97 link is busy while PCM data is being transmitted.
+  handleHeadphones();
   audioSetVolume(31, 31);
   
   for(int i = 0 ; i < 500 ; i += 50) {
